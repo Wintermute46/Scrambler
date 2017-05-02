338c9563623e701db384330cbbb8237b23b06cd5
Wintermute46 <r.tzymbalyuk@yandex.ru>
2017-05-02 12:59:25 +0400

    Add Base64 encode to crypt

--- app/src/main/java/m2a/com/scrambler/MainActivity.java
+++ app/src/main/java/m2a/com/scrambler/MainActivity.java
@@ -3,6 +3,7 @@ package m2a.com.scrambler;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,21 +25,31 @@ public class MainActivity extends AppCompatActivity {
 
         byte[] secText = eSec.getText().toString().getBytes();  //взяли то, что введено в секретный текст
         byte[] key = eKey.getText().toString().getBytes();      //взяли ключ
-        byte[] crypText = new byte[secText.length];             //шифр и его длина
-        //String secText = eSec.getText().toString();     //взяли то, что введено в секретный текст
-        //String key = eKey.getText().toString();         //взяли ключ
-        //String crypText = "";
 
-        for (int i = 0; i < secText.length; i++) {        //идем по секретному тексту
+        if ( (secText.length != 0) & (key.length !=0) ) {
 
-            crypText[i] = (byte)(secText[i] ^ key[i % key.length]);   //ксорим одно с другим
+            byte[] secTextBase = Base64.encode(secText, Base64.DEFAULT);
+            //BASE64Encoder enc = new BASE64Encoder();
 
+            byte[] crypText = new byte[secTextBase.length];             //шифр и его длина
+            //String secText = eSec.getText().toString();     //взяли то, что введено в секретный текст
+            //String key = eKey.getText().toString();         //взяли ключ
+            //String crypText = "";
+
+            for (int i = 0; i < secTextBase.length; i++) {        //идем по секретному тексту
+
+                crypText[i] = (byte) (secTextBase[i] ^ key[i % key.length]);   //ксорим одно с другим
+
+            }
+
+            //пилим передатчик
+            Intent intent = new Intent(this, CryptActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, new String(crypText));
+            startActivity(intent);      //давай досвидос!
+
+        } else {
+            //TODO: какую-нибудь ругалочку
         }
-
-        //пилим передатчик
-        Intent intent = new Intent(this, CryptActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, new String(crypText));
-        startActivity(intent);      //давай досвидос!
 
     }
 
